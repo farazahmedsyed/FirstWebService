82415e4f625964749f0aee132a8e23332dcd39ad
farazahmedsyed <syedfaraz_ahmed@ymail.com>
2017-02-20 11:13:00 +0500

    FC

--- src/main/java/com/jms/MessageSender.java
+++ src/main/java/com/jms/MessageSender.java
@@ -10,22 +10,23 @@ import java.util.Properties;
  * Created by Venturdive on 20/02/2017.
  */
 public class MessageSender {
-    private static final String USER = "admin";
+    private static final String USER = "guestUser";
     private static final String PASSWORD = "password";
 
     public static void main(String args[]) {
         Properties env = new Properties();
         env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
-        env.put(Context.PROVIDER_URL, "remote://localhost:8080");
+        env.put(Context.PROVIDER_URL, "http-remoting://localhost:8080");
         env.put(Context.SECURITY_PRINCIPAL, USER);
         env.put(Context.SECURITY_CREDENTIALS, PASSWORD);
 
         try {
             Context ctx = new InitialContext(env);
-            ConnectionFactory factory = (ConnectionFactory) ctx.lookup("java:/ConnectionFactory");
+            //http-remoting by default search in java:jboss/exported/ , hence complete path is java:/jboss/exported/jms/RemoteConnectionFactory
+            ConnectionFactory factory = (ConnectionFactory) ctx.lookup("jms/RemoteConnectionFactory");
             Connection connection = factory.createConnection(USER, PASSWORD);
             Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            Destination dest = (Destination) ctx.lookup("java:/jms/queue/ExpiryQueue");
+            Destination dest = (Destination) ctx.lookup("jms/queue/testQ");
 
             MessageProducer sender = session.createProducer(dest);
             TextMessage message = session.createTextMessage("Hello World JMS");
